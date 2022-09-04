fe326dcf8ff32d2ec800eedf38b4c6b54d0e636f
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2022-09-04 15:36:12 +0100

    Create A*I.py

---
+++ A*I.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[52]:
+
+
+# !pip install pyttsx3
+import pyttsx3
+import speech_recognition as sr
+import webbrowser
+import datetime
+import pywhatkit
+import os
+import yfinance as yf
+import pyjokes
+import wikipedia
+
+
+# In[2]:
+
+
+#listen to our microphone and return the audio as text using google
